c5935aa5fd49ffc078d6a672c59f8668f4d2209f
triet <minhtriet@bacoor.co>
2021-12-03 09:39:22 +0700

    update UI network select

--- src/components/NetworkSelectorBacoorModal/NetworkSelectorBacoor.tsx
+++ src/components/NetworkSelectorBacoorModal/NetworkSelectorBacoor.tsx
@@ -4,8 +4,12 @@ import ethereumLogo from 'assets/images/ethereum-logo.png'
 import { AutoColumn } from 'components/Column'
 import { AutoRow, RowBetween } from 'components/Row'
 import { PaddedColumn } from 'components/SearchModal/styleds'
+import { CHAIN_INFO, SupportedChainId } from 'constants/chains'
+import { useActiveWeb3React } from 'hooks/web3'
+import { useAppSelector } from 'state/hooks'
 import styled from 'styled-components/macro'
 import { CloseIcon, TYPE } from 'theme'
+import { switchToNetwork } from 'utils/switchToNetwork'
 
 const Wrapper = styled.div`
   position: relative;
@@ -53,12 +57,13 @@ const NetworkContainer = styled.div`
   grid-column: 1 / span 2;
   grid-template-columns: 1fr 1fr 1fr;
 `
-const NetworkItem = styled.div`
+const NetworkItem = styled.div<{ active: boolean }>`
   display: flex;
   flex-direction: column;
   align-items: center;
   padding: 8px;
   border-radius: 16px;
+  cursor: pointer;
 `
 const SelectedIconWrapper = styled.div`
   position: absolute;
@@ -91,6 +96,35 @@ interface ImportProps {
 
 export function NetworkSelectorBacoor(props: ImportProps) {
   const { onDismiss } = props
+  const { chainId, library } = useActiveWeb3React()
+  const implements3085 = useAppSelector((state) => state.application.implements3085)
+
+  function NetworkElement({ targetChain }: { targetChain: number }) {
+    if (!library || !chainId || (!implements3085 && targetChain !== chainId)) {
+      return null
+    }
+    const handleElementClick = () => {
+      switchToNetwork({ library, chainId: targetChain })
+    }
+    const active = chainId === targetChain
+    const isOptimism = targetChain === SupportedChainId.OPTIMISM
+    const networkName = `${CHAIN_INFO[targetChain].label}${isOptimism ? ' (Optimism)' : ''}`
+    const ElementContent = () => (
+      <NetworkItem onClick={handleElementClick} active={active}>
+        <NetworkImgWrap>
+          <NetworkImg src={CHAIN_INFO[targetChain].logoUrl}></NetworkImg>
+          {active && (
+            <SelectedIconWrapper>
+              <SelectedIcon src={doneIcon}></SelectedIcon>
+            </SelectedIconWrapper>
+          )}
+        </NetworkImgWrap>
+        <SubText>{networkName}</SubText>
+      </NetworkItem>
+    )
+
+    return <ElementContent />
+  }
   return (
     <Wrapper>
       <PaddedColumn gap="14px" style={{ width: '100%', flex: '1 1', paddingBottom: 0 }}>
@@ -127,48 +161,12 @@ export function NetworkSelectorBacoor(props: ImportProps) {
       </AutoColumn>
       <AutoColumn gap="md" style={{ padding: '0 1rem 1rem 1rem' }}>
         <NetworkContainer>
-          <NetworkItem>
-            <NetworkImgWrap>
-              <NetworkImg src={ethereumLogo}></NetworkImg>
-              <SubText>Ethereum</SubText>
-            </NetworkImgWrap>
-          </NetworkItem>
-          <NetworkItem>
-            <NetworkImgWrap>
-              <NetworkImg src={ethereumLogo}></NetworkImg>
-              <SelectedIconWrapper>
-                <SelectedIcon src={doneIcon}></SelectedIcon>
-              </SelectedIconWrapper>
-            </NetworkImgWrap>
-            <SubText>Ethereum</SubText>
-          </NetworkItem>
-          <NetworkItem>
-            <NetworkImgWrap>
-              <NetworkImg src={ethereumLogo}></NetworkImg>
-            </NetworkImgWrap>
-            <SubText>Ethereum</SubText>
-          </NetworkItem>
-          <NetworkItem>
-            <NetworkImgWrap>
-              <NetworkImg src={ethereumLogo}></NetworkImg>
-            </NetworkImgWrap>
-            <SubText>Ethereum</SubText>
-          </NetworkItem>
-          <NetworkItem>
-            <NetworkImgWrap>
-              <NetworkImg src={ethereumLogo}></NetworkImg>
-            </NetworkImgWrap>
-            <SubText>Ethereum</SubText>
-          </NetworkItem>
-          <NetworkItem>
-            <NetworkImgWrap>
-              <NetworkImg src={ethereumLogo}></NetworkImg>
-            </NetworkImgWrap>
-            <SubText>Ethereum</SubText>
-          </NetworkItem>
+          <NetworkElement targetChain={SupportedChainId.MAINNET} />
+          <NetworkElement targetChain={SupportedChainId.POLYGON_TESTNET} />
+          <NetworkElement targetChain={SupportedChainId.TOMOCHAIN_TESNET} />
         </NetworkContainer>
       </AutoColumn>
-      <AutoColumn gap="md" style={{ padding: '0 1rem 1rem 1rem' }}>
+      {/* <AutoColumn gap="md" style={{ padding: '0 1rem 1rem 1rem' }}>
         <StepContainer>
           <WalletConnectStep>3</WalletConnectStep>
           <BodyText>
@@ -178,92 +176,9 @@ export function NetworkSelectorBacoor(props: ImportProps) {
       </AutoColumn>
       <AutoColumn gap="md" style={{ padding: '0 1rem 1rem 1rem' }}>
         <NetworkContainer>
-          <NetworkItem>
-            <NetworkImgWrap>
-              <NetworkImg src={ethereumLogo}></NetworkImg>
-              <SubText>Ethereum</SubText>
-            </NetworkImgWrap>
-          </NetworkItem>
-          <NetworkItem>
-            <NetworkImgWrap>
-              <NetworkImg src={ethereumLogo}></NetworkImg>
-            </NetworkImgWrap>
-            <SubText>Ethereum</SubText>
-          </NetworkItem>
-          <NetworkItem>
-            <NetworkImgWrap>
-              <NetworkImg src={ethereumLogo}></NetworkImg>
-            </NetworkImgWrap>
-            <SubText>Ethereum</SubText>
-          </NetworkItem>
-          <NetworkItem>
-            <NetworkImgWrap>
-              <NetworkImg src={ethereumLogo}></NetworkImg>
-            </NetworkImgWrap>
-            <SubText>Ethereum</SubText>
-          </NetworkItem>
-          <NetworkItem>
-            <NetworkImgWrap>
-              <NetworkImg src={ethereumLogo}></NetworkImg>
-            </NetworkImgWrap>
-            <SubText>Ethereum</SubText>
-          </NetworkItem>
-          <NetworkItem>
-            <NetworkImgWrap>
-              <NetworkImg src={ethereumLogo}></NetworkImg>
-            </NetworkImgWrap>
-            <SubText>Ethereum</SubText>
-          </NetworkItem>
-          <NetworkItem>
-            <NetworkImgWrap>
-              <NetworkImg src={ethereumLogo}></NetworkImg>
-            </NetworkImgWrap>
-            <SubText>Ethereum</SubText>
-          </NetworkItem>
-          <NetworkItem>
-            <NetworkImgWrap>
-              <NetworkImg src={ethereumLogo}></NetworkImg>
-            </NetworkImgWrap>
-            <SubText>Ethereum</SubText>
-          </NetworkItem>
-          <NetworkItem>
-            <NetworkImgWrap>
-              <NetworkImg src={ethereumLogo}></NetworkImg>
-            </NetworkImgWrap>
-            <SubText>Ethereum</SubText>
-          </NetworkItem>
-          <NetworkItem>
-            <NetworkImgWrap>
-              <NetworkImg src={ethereumLogo}></NetworkImg>
-            </NetworkImgWrap>
-            <SubText>Ethereum</SubText>
-          </NetworkItem>
-          <NetworkItem>
-            <NetworkImgWrap>
-              <NetworkImg src={ethereumLogo}></NetworkImg>
-            </NetworkImgWrap>
-            <SubText>Ethereum</SubText>
-          </NetworkItem>
-          <NetworkItem>
-            <NetworkImgWrap>
-              <NetworkImg src={ethereumLogo}></NetworkImg>
-            </NetworkImgWrap>
-            <SubText>Ethereum</SubText>
-          </NetworkItem>
-          <NetworkItem>
-            <NetworkImgWrap>
-              <NetworkImg src={ethereumLogo}></NetworkImg>
-            </NetworkImgWrap>
-            <SubText>Ethereum</SubText>
-          </NetworkItem>
-          <NetworkItem>
-            <NetworkImgWrap>
-              <NetworkImg src={ethereumLogo}></NetworkImg>
-            </NetworkImgWrap>
-            <SubText>Ethereum</SubText>
-          </NetworkItem>
+          
         </NetworkContainer>
-      </AutoColumn>
+      </AutoColumn> */}
     </Wrapper>
   )
 }
