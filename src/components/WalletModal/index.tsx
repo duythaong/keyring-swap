67b122eda77827d49f38d7fcae7ebb210cb875fb
triet <minhtriet@bacoor.co>
2021-12-21 15:01:56 +0700

    update

--- src/components/WalletModal/index.tsx
+++ src/components/WalletModal/index.tsx
@@ -360,7 +360,7 @@ export default function WalletModal({
           <ContentWrapper>
             {error instanceof UnsupportedChainIdError ? (
               <h5>
-                <Trans>{`Please connect to the appropriate ${chainInfo?.label} network.`}</Trans>
+                <Trans>{`Please connect to the appropriate Ethereum network.`}</Trans>
               </h5>
             ) : (
               <Trans>Error connecting. Try refreshing the page.</Trans>
